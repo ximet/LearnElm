99399800faa5303877be53e81e9b9aa5ffae2989
ximet <dq.ximet@gmail.com>
2016-11-26 21:01:43 +0300

    First function and call

--- hello/Main.elm
+++ hello/Main.elm
@@ -3,4 +3,21 @@ module Main exposing (..)
 -- for install use command: elm package install elm-lang/html
 import Html exposing (text)
 
-main = text "Hello"
+politely : String -> String
+politely phrase =
+  "Excuse me, " ++ phrase
+
+ask : String -> String -> String
+ask thing place =
+  "is there a "
+  ++ thing
+  ++ " in the "
+  ++ place
+  ++ "?"
+
+
+askPolitelyAboutFish : String -> String
+askPolitelyAboutFish = politely << (ask "fish")
+
+main =
+  text <| askPolitelyAboutFish "river"
